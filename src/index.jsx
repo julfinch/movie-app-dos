75a79a9f30ce5125877506cc43b067561cbea0f7
julfinch <julfinch@gmail.com>
2022-07-09 01:26:39 +0800

    4th commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <Router>
     <App />
-  </BrowserRouter>
+  </Router>
 );
+
